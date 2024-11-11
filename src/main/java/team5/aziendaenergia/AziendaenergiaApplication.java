809fca40afc8000f012e2fa92f773ece054553f7
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-11-11 15:12:26 +0100

    Initial commit

---
+++ src/main/java/team5/aziendaenergia/AziendaenergiaApplication.java
@@ -0,0 +1,13 @@
+package team5.aziendaenergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AziendaenergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AziendaenergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team5/aziendaenergia/AziendaenergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.aziendaenergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AziendaenergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
