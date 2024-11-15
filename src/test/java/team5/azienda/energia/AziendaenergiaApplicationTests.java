6b440a48aad65f6b8e39a0936ee3ee93a8ebc9f1
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-15 15:31:08 +0100

    modificati nomi variabili

--- src/test/java/team5/azienda/energia/AziendaenergiaApplicationTests.java
+++ src/test/java/team5/azienda/energia/AziendaenergiaApplicationTests.java
@@ -1,15 +1,11 @@
 package team5.azienda.energia;
 
-import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import team5.azienda.energia.entities.Provincia;
 import team5.azienda.energia.services.ProvinciaService;
-
-import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,10 +16,10 @@ class AziendaenergiaApplicationTests {
 
     @ParameterizedTest
     @CsvSource({"Erchie,Brindisi ", "Malalbergo, Bologna", " Vignola, Modena", "Bussolengo, Verona", "Rutigliano, Bari ", "Comacchio, Ferrara"})
-    void Provinciamune(String nomeComune, String provincia) {
+    void provinciaComune(String nomeComune, String provincia) {
         Provincia p = ps.findByComune(nomeComune);
-        String nomePrpvicnia = p.getNome();
-        assertEquals(provincia, nomePrpvicnia);
+        String nomeProvincia = p.getNome();
+        assertEquals(provincia, nomeProvincia);
     }
 
 }
