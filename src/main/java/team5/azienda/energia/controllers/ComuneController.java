63fcb5b912b6a15a4175b6607a5da8f5af6a4fe7
Gianluca Cerreta <gianluca2003ba@gmail.com>
2024-11-15 10:24:31 +0100

    corretto da "servicies" a "services"

--- src/main/java/team5/azienda/energia/controllers/ComuneController.java
+++ src/main/java/team5/azienda/energia/controllers/ComuneController.java
@@ -1,14 +1,13 @@
 package team5.azienda.energia.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import team5.azienda.energia.entities.Comune;
 import team5.azienda.energia.entities.Provincia;
-import team5.azienda.energia.servicies.ComuneService;
-import team5.azienda.energia.servicies.ProvinciaService;
+import team5.azienda.energia.services.ComuneService;
+import team5.azienda.energia.services.ProvinciaService;
 
 import java.io.IOException;
 
@@ -21,7 +20,7 @@ public class ComuneController {
 
     @GetMapping("/comuni")
     public Page<Comune> findAllComuni(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "100") int size,
-                                @RequestParam(defaultValue = "nome") String sortBy) {
+                                      @RequestParam(defaultValue = "nome") String sortBy) {
         return this.cs.findAll(page, size, sortBy);
     }
 
