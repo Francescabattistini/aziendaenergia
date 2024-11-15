63ddd5dc2ad6d529cf7ef516cea876fd47177560
MangeleLoic <behonloic@gmail.com>
2024-11-15 08:44:11 +0100

    adds queries clienti

--- src/main/java/team5/azienda/energia/repositories/ClienteRepo.java
+++ src/main/java/team5/azienda/energia/repositories/ClienteRepo.java
@@ -1,9 +1,17 @@
 package team5.azienda.energia.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import team5.azienda.energia.entities.Cliente;
 
-@Repository
 public interface ClienteRepo extends JpaRepository<Cliente, Long> {
+    Page<Cliente> findByFatturatoAnnualeGreaterThanEqual(Double fatturatoAnnuale, Pageable pageable);
+
+    Page<Cliente> findByDataInserimento(LocalDate dataInserimento, Pageable pageable);
+
+    Page<Cliente> findByDataUltimoContatto(LocalDate dataUltimoContatto, Pageable pageable);
+
+    Page<Cliente> findByNomeContattoContainingIgnoreCase(String parteNome, Pageable pageable);
 }
+
